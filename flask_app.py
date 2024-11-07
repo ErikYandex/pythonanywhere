0dafd833d98d0281f05a36c632111e513e6df381
Erik <erikstankevich@yandex.com>
2024-11-07 09:12:06 +0100

    Add day to app

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,16 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
+from datetime import datetime
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'foobar, world!'
+
+	DAYNAMES = ["Monday", Tuesday", "Wednesday", "Thursday", Friday", "Saturday", "Sunday"]
+dayname = DAYNAMES[datetime.now().weekday()]
+    return f"<p>'foobar, world! Happy {dayname}.</p>'
 
